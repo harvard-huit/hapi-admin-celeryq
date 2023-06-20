bf4a1a79877b278ed520199f20a4bd6621f67f7e
Mark Stacy <mark_stacy@harvard.edu>
2023-06-20 16:26:42 -0400

    update install requires

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup, find_packages
 setup(name='adexk8s-migration-celeryq',
       version='0.0',
       packages= find_packages(),
+      install_requires=[
+          'google-api-python-client==2.88.0',
+      ],
 )
